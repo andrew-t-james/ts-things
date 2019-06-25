fbb0e0abc5a1c84dca262c731442a35e59c1597d
Andrew James <ajames20@gmail.com>
2019-06-25 04:14:08 -0500

    Add basic html report

--- csv/src/index.ts
+++ csv/src/index.ts
@@ -2,15 +2,13 @@ import MatchReader from './MatchReader';
 import CsvFileReader from './CsvFileReader';
 import WinsAnalysis from './analyzers/WinsAnalysis';
 import ConsoleReport from './resultTarget/ConsolReporter';
+import HtmlReport from './resultTarget/HtmlReport';
 import Summary from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const reader = new MatchReader(csvFileReader);
 reader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPrintReport(reader.matches);
