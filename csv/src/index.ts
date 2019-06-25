60eaa96be482aae1028fdd27b8bb2f715d1c544d
Andrew James <ajames20@gmail.com>
2019-06-25 04:18:16 -0500

    Add static method to Summary

--- csv/src/index.ts
+++ csv/src/index.ts
@@ -9,6 +9,6 @@ const csvFileReader = new CsvFileReader('football.csv');
 const reader = new MatchReader(csvFileReader);
 reader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
 summary.buildAndPrintReport(reader.matches);
