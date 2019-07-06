116158733b98246b8bc29861bf087041cc4b0719
Andrew James <ajames20@gmail.com>
2019-07-06 18:24:51 -0500

    Add Sync and generics

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,14 +1,17 @@
-import axios, { AxiosResponse } from 'axios';
 import Eventing from './Eventing';
+import Sync from './Sync';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
   constructor(private data: UserProps) {}
 
@@ -18,23 +21,6 @@ class User {
 
   set(update: UserProps): void {
     this.data = { ...this.data, ...update };
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((res: AxiosResponse): void => {
-        this.set(res.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
   }
 }
 
