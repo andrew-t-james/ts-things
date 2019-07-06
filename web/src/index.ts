c013e034a49d05983d096cdabf95998289b033b4
Andrew James <ajames20@gmail.com>
2019-07-06 17:40:25 -0500

    Add uses validation on save

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,13 @@
 import User from './models/User';
 
 const newUser = new User({ id: 1 });
-newUser.fetch();
+const newUserTwo = new User({ name: 'joben', age: 36 });
+newUser.set({ name: 'hank mardukus', age: 38 });
+newUser.save();
+// newUserTwo.fetch();
+newUserTwo.save();
 
 setTimeout(() => {
   console.log(newUser);
+  console.log(newUserTwo);
 }, 4000);
