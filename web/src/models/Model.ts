7f13761aba554bc20fb0df5bd915c0932c4bc579
Andrew James <ajames20@gmail.com>
2019-07-07 13:20:59 -0500

    Refactor user

---
+++ web/src/models/Model.ts
@@ -0,0 +1,72 @@
+import { AxiosPromise, AxiosResponse } from 'axios';
+
+interface ModelAttributes<T> {
+  set(update: T): void;
+  getAll(): T;
+  get<K extends keyof T>(key: K): T[K];
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+
+interface Events {
+  on(event: string, callBack: Callback): void;
+  trigger(event: string): void;
+}
+
+interface HasId {
+  id?: number;
+}
+
+type Callback = () => void;
+
+class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) {}
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: T) {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw Error('Cannot fetch without id');
+    }
+
+    this.sync.fetch(id).then((res: AxiosResponse): void => {
+      this.set(res.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      .then((res: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => this.trigger('error'));
+  }
+}
+
+export default Model;
