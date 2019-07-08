93afd93322503725be682e74db8783844fd98dab
Andrew James <ajames20@gmail.com>
2019-07-08 06:53:26 -0500

    Add initial express setup

---
+++ express/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(router);
+
+// eslint-disable-next-line no-console
+app.listen(3000, () => console.log('Listening on port 3000'));
