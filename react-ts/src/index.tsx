645e49da4d60e6c1af545a4366796f5efe0d1424
Andrew James <ajames20@gmail.com>
2019-08-04 17:50:58 -0500

    Add react ts

---
+++ react-ts/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+function App() {
+  return (
+    <div>hello</div>
+  )
+}
+
+ReactDom.render(<App/>, document.querySelector('#root'))
